77c2ccfc1a58eae3b0f13585b2d2b11e42f9eb50
Amirhossein Shahabnia <amir@stevenslateaudio.com>
2024-05-17 16:41:27 -0700

    adds sang glitch logo to not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,9 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function NotFound() {
   return (
     <main className="min-h-screen flex justify-center flex-col mx-auto w-11/12 lg:max-w-4xl xl:max-w-6xl 2xl:max-w-7x">
+      <div className="glitch-logo mb-4"></div>
       <h2 className="text-3xl mb-2">404 | Not Found</h2>
       <p>Could not find the requested resource</p>
       <Link
