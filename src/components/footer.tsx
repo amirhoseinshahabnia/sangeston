d52bf14179cb5401fe3440c5a3d10b2109f718d7
Amirhossein Shahabnia <amir@stevenslateaudio.com>
2024-06-24 13:03:50 -0700

    adds contact id to footer for main nav anchor scroll

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -10,7 +10,10 @@ const Footer = ({ data }: Props) => {
   const date = new Date();
   return (
     <footer id="footer" className="pb-4 pt-16 xl:pt-20 2xl:pt-24 relative">
-      <div className="wrapper main-color flex flex-col mx-auto w-11/12 lg:max-w-4xl xl:max-w-6xl 2xl:max-w-7x">
+      <div
+        className="wrapper main-color flex flex-col mx-auto w-11/12 lg:max-w-4xl xl:max-w-6xl 2xl:max-w-7x"
+        id="contact"
+      >
         <div className="get-in-touch mb-32 xl:mb-40 2xl:mb-48">
           <Typography
             variant="h2"
