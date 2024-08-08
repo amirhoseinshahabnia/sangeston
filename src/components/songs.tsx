150417c3d290abf146daa5b334ccb77768d3b0da
Amirhossein Shahabnia <amir@stevenslateaudio.com>
2024-08-08 13:46:21 -0700

    updates stylings + scroll functionality to be based on window innderWidth

--- src/components/songs.tsx
+++ src/components/songs.tsx
@@ -1,9 +1,12 @@
 "use client";
 
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import Song from "./song";
 
+const MIN_SCREEN_WIDTH_FOR_SCROLL = 1024;
+
 const Songs = ({ songs }: { songs: any }) => {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
   const songsRef = useRef<HTMLDivElement>(null);
   const activeTimeline = useRef<HTMLDivElement>(null);
 
@@ -11,42 +14,50 @@ const Songs = ({ songs }: { songs: any }) => {
   useEffect(() => {
     let allYearsIndicators = document.querySelectorAll(".year-ctn");
     function handleScroll(e: any) {
-      if (songsRef.current && activeTimeline.current) {
-        const tlrRefRect = songsRef.current.getBoundingClientRect();
-        const activeTLRect = activeTimeline.current.getBoundingClientRect();
-        if (tlrRefRect.y > 180) {
-          activeTimeline.current.style.transform = "translateY(0)";
-        } else if (
-          tlrRefRect.y <= 180 &&
-          tlrRefRect.y >= -1 * tlrRefRect.height
-        ) {
-          // move the active tl
-          activeTimeline.current.style.transform = `translateY(${
-            -1 * tlrRefRect.y + 180
-          }px)`;
-        }
-
-        allYearsIndicators.forEach((year) => {
-          if (
-            year.getBoundingClientRect().y <=
-              activeTLRect.y + activeTLRect.height - 220 &&
-            year.getBoundingClientRect().y > -activeTLRect.height / 3
+      if (screenWidth >= MIN_SCREEN_WIDTH_FOR_SCROLL) {
+        if (songsRef.current && activeTimeline.current) {
+          const tlrRefRect = songsRef.current.getBoundingClientRect();
+          const activeTLRect = activeTimeline.current.getBoundingClientRect();
+          if (tlrRefRect.y > 180) {
+            activeTimeline.current.style.transform = "translateY(0)";
+          } else if (
+            tlrRefRect.y <= 180 &&
+            tlrRefRect.y >= -1 * tlrRefRect.height
           ) {
-            year.classList.add("active");
-          } else {
-            year.classList.remove("active");
+            // move the active tl
+            activeTimeline.current.style.transform = `translateY(${
+              -1 * tlrRefRect.y + 180
+            }px)`;
           }
-        });
+
+          allYearsIndicators.forEach((year) => {
+            if (
+              year.getBoundingClientRect().y <=
+                activeTLRect.y + activeTLRect.height - 220 &&
+              year.getBoundingClientRect().y > -activeTLRect.height / 3
+            ) {
+              year.classList.add("active");
+            } else {
+              year.classList.remove("active");
+            }
+          });
+        }
       }
     }
 
+    function handleResize() {
+      setScreenWidth(window.innerWidth);
+    }
+
     document.addEventListener("scroll", handleScroll);
+    window.addEventListener("resize", handleResize);
 
     // clean up
-    () => {
-      return document.removeEventListener("scroll", handleScroll);
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("resize", handleResize);
     };
-  }, []);
+  }, [screenWidth]);
 
   return (
     <div className="songs py-20 lg:py-44" ref={songsRef}>
