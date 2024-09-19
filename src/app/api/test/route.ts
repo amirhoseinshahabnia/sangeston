7906d29ad72424242c897531c3a8b20d3e3a3c47
Amirhossein Shahabnia <amir@stevenslateaudio.com>
2024-09-19 08:11:50 -0700

    updates api route

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   // @ts-ignore
-  console.log(request.body.customer);
+  console.log(request.body);
 
   return NextResponse.json({ msg: "Webhook received!" });
 }
