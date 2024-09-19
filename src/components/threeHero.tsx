ac6e60bfa33792801acb0871ce80b34585598fc6
Amirhossein Shahabnia <amir@stevenslateaudio.com>
2024-09-19 12:54:55 -0700

    fixes bug

--- src/components/threeHero.tsx
+++ src/components/threeHero.tsx
@@ -27,93 +27,99 @@ function ThreeHero() {
     }
   };
 
-  // === THREE.JS CODE START ===
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    100
-  );
-  camera.position.set(0, 0, 3);
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-
-  let environmentTexture: THREE.DataTexture;
-
-  new RGBELoader().load(hdr, (texture) => {
-    environmentTexture = texture;
-    environmentTexture.mapping = THREE.EquirectangularReflectionMapping;
-    scene.environment = environmentTexture;
-    scene.background = environmentTexture;
-    //   scene.backgroundBlurriness = 0.01;
-    scene.environmentIntensity = 1;
-    setTimeout(() => {
-      setLoading(false);
-    }, 1200);
-  });
-
-  const material = new THREE.MeshPhysicalMaterial();
-  material.side = THREE.DoubleSide;
-  material.roughness = 0;
-  material.metalness = 1;
-
-  const directionallight = new THREE.DirectionalLight(0xebfeff, Math.PI);
-  directionallight.position.set(1, 0.1, 1);
-  directionallight.visible = false;
-  scene.add(directionallight);
-
-  const ambientLight = new THREE.AmbientLight(0xebfeff, Math.PI / 16);
-  ambientLight.visible = true;
-  scene.add(ambientLight);
-
-  let obj: any;
-
-  new GLTFLoader().load(model, (gltf) => {
-    gltf.scene.traverse((child) => {
-      (child as THREE.Mesh).material = material;
-    });
-    obj = gltf.scene;
-    scene.add(gltf.scene);
-  });
-
-  // use ref as a mount point of the Three.js scene instead of the document.body
-  refContainer.current && refContainer.current.appendChild(renderer.domElement);
-
-  document.addEventListener("resize", () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+  useEffect(() => {
+    // === THREE.JS CODE START ===
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      100
+    );
+    camera.position.set(0, 0, 3);
+    const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
-  });
 
-  document.addEventListener("mousemove", (e) => {
-    if (window.innerWidth > 768) {
-      obj.rotation.x = THREE.MathUtils.lerp(0, e.clientY, 0.001);
-      obj.rotation.y = THREE.MathUtils.lerp(0, e.clientX, 0.002);
-      obj.rotation.z = THREE.MathUtils.lerp(0, e.clientY, 0.0001);
+    let environmentTexture: THREE.DataTexture;
+
+    new RGBELoader().load(hdr, (texture) => {
+      environmentTexture = texture;
+      environmentTexture.mapping = THREE.EquirectangularReflectionMapping;
+      scene.environment = environmentTexture;
+      scene.background = environmentTexture;
+      //   scene.backgroundBlurriness = 0.01;
+      scene.environmentIntensity = 1;
+      setTimeout(() => {
+        setLoading(false);
+      }, 1200);
+    });
+
+    const material = new THREE.MeshPhysicalMaterial();
+    material.side = THREE.DoubleSide;
+    material.roughness = 0;
+    material.metalness = 1;
+
+    const directionallight = new THREE.DirectionalLight(0xebfeff, Math.PI);
+    directionallight.position.set(1, 0.1, 1);
+    directionallight.visible = false;
+    scene.add(directionallight);
+
+    const ambientLight = new THREE.AmbientLight(0xebfeff, Math.PI / 16);
+    ambientLight.visible = true;
+    scene.add(ambientLight);
+
+    let obj: any;
+
+    new GLTFLoader().load(model, (gltf) => {
+      gltf.scene.traverse((child) => {
+        (child as THREE.Mesh).material = material;
+      });
+      obj = gltf.scene;
+      scene.add(gltf.scene);
+    });
+
+    // use ref as a mount point of the Three.js scene instead of the document.body
+    refContainer.current &&
+      refContainer.current.appendChild(renderer.domElement);
+
+    window.addEventListener("resize", () => {
+      if (window.innerWidth > 768) {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+      }
+    });
+
+    document.addEventListener("mousemove", (e) => {
+      if (window.innerWidth > 768 && obj) {
+        obj.rotation.x = THREE.MathUtils.lerp(0, e.clientY, 0.001);
+        obj.rotation.y = THREE.MathUtils.lerp(0, e.clientX, 0.002);
+        obj.rotation.z = THREE.MathUtils.lerp(0, e.clientY, 0.0001);
+      }
+    });
+
+    function animate() {
+      requestAnimationFrame(animate);
+
+      if (window.innerWidth <= 768 && obj) {
+        obj.rotation.y += 0.01;
+      }
+
+      renderer.render(scene, camera);
     }
-  });
-
-  function animate() {
-    requestAnimationFrame(animate);
-
-    if (window.innerWidth <= 768 && obj) {
-      obj.rotation.y += 0.01;
-    }
-
-    renderer.render(scene, camera);
-  }
-  animate();
+    animate();
+  }, []);
 
   return (
     <>
-      <div ref={refContainer}></div>
       {loading && (
         <div className="hero-loader min-h-screen flex justify-center flex-col items-center">
           <div className="glitch-logo mb-4"></div>
           <p>Loading...</p>
         </div>
       )}
+      <div ref={refContainer}></div>
+
       <div
         className="scroll-hero main-color absolute bottom-1  flex flex-col items-center hover:opacity-80 cursor-pointer"
         onClick={handleScrollClick}
